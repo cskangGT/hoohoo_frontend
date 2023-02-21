39c21b1c795277f6da701e949c32ae34a727db46
Moungsung <mslim1210@gmail.com>
2023-02-21 14:57:12 -0500

    Conflict resolved

--- Diary_app/src/screens/TagRecording/TagRecording.tsx
+++ Diary_app/src/screens/TagRecording/TagRecording.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { View, Text, Button, StyleSheet } from "react-native";
+import React, { useEffect, useRef, useState } from 'react'
+import { View, Text, Button, StyleSheet, TextInput, TouchableOpacity, Pressable, Keyboard } from "react-native";
 import styled from 'styled-components';
 // import styles from '.././styles'
 import msg from '..//data/msg.json'
@@ -9,9 +9,39 @@ const StyledButtonContainer = styled(View)`
     align-items: center;
     flex-wrap: wrap;
     `
+// justify-content: center;
+// align-items: center; 밑에 필요에따라 좌우 정하소 
+const StyledModifyContainer = styled(View)`
+   
+    border-width: 1;
+    width: 100%;
+    border-color: grey;
+    `
+const StyledModifyButton = styled(TouchableOpacity)`
+    border-width: 1;
+    border-color: white;
+    border-radius: 50;    
+    background-color: pink;
+    padding: 10px;
+    width:80px;
+    justify-content: center;
+    align-items: center;
+    `
+const StyledModifyText = styled(Text)`
+    color: white;
+    `
 
-
-
+function ModifyContainer(props: any & string): JSX.Element {
+    return (
+        <StyledModifyContainer >
+            <StyledModifyButton>
+                <StyledModifyText>
+                    {props.text}
+                </StyledModifyText>
+            </StyledModifyButton>
+        </StyledModifyContainer>
+    )
+}
 function WordContainer(content: any & JSX.Element): JSX.Element {
     return (
         <StyledButtonContainer>
@@ -22,7 +52,72 @@ function WordContainer(content: any & JSX.Element): JSX.Element {
     )
 }
 
+function UserTextInput(props: any): JSX.Element {
+    //focus if current mode is type mode which contains Speech Mode button
+    let focus = (props.currTypeButton === "Speech Mode")
+    return (
+
+        <TextInput
+            style={{ height: 100, width: '100%', borderWidth: 1, padding: 10 }}
+            onFocus={() => {
+                if (props.currTypeButton === "Type Mode") {
+                    props.switchMode()
+                }
+            }
+            }
+            pointerEvents="none"
+            ref={props.focusOnInput}
+            onChangeText={(text: string) => props.onChangeText(text)}
+            value={props.textInput}
+            fontSize={44}
+            autoFocus={focus}
+            blurOnSubmit={false} //disable dismissing keyboard panel automatically!
+            onSubmitEditing={() => {
+                props.recordTags(props.textInput as string); //reuse previosuly made function for STT input!
+                props.setTextInput("");
+            }} />
+
+    );
+}
+function ModeContentComponents(props: any): JSX.Element {
+    return (
+        <View style={{ width: '100%' }}>
+            <UserTextInput
+                textInput={props.textInput} onChangeText={props.onChangeText} recordTags={props.recordTags}
+                setTextInput={props.setTextInput} currTypeButton={props.currTypeButton} switchMode={props.switchMode}
+                focusOnInput={props.focusOnInput}
+            ></UserTextInput>
+            <View style={{
+                justifyContent: 'flex-start',
+                alignItems: 'flex-start',
+                width: '100%',
+            }}>
+                <Button
+                    style={{
+                        textAlign: 'left',
+                        borderWidth: 1,
+                        borderColor: 'orange',
+                        borderRadius: 50,
+                        padding: 10,
+                        backgroundColor: 'transparent',
+                        margin: 5
+                    }}
+                    color='orange'
+                    title={props.currTypeButton}
+                    onPress={props.switchMode} />
+            </View>
+        </View>
+    )
+}
+function ModeContentContainer(props: any): JSX.Element {
+    return (
+        <View style={{ width: '100%' }}>
+            {props.content}
+        </View>)
+}
 function TagRecording(): JSX.Element {
+
+
     const [inputs, setInputs] = useState<string[]>([])
     const [InputContentHolder, setInputContentHolder] = useState<JSX.Element[]>()
     const [recordedInputs, setRecordedInputs] = useState<string[]>([])
@@ -37,13 +132,24 @@ function TagRecording(): JSX.Element {
     const createContent = (words: string[], setWords: React.Dispatch<React.SetStateAction<string[]>>, setWordContent: React.Dispatch<React.SetStateAction<JSX.Element[] | undefined>>) => {
         let contentHolder = (
             words.map((word: string, index: number) => (
-                <Button
-                    
-                    color={"grey"}
+                // <View style={{ backgroundColor: 'transparent' }}>
+                <TouchableOpacity
+                    style={{
+                        borderWidth: 1,
+                        borderColor: 'orange',
+                        borderRadius: 50,
+                        padding: 10,
+                        backgroundColor: 'transparent',
+                        margin: 5
+                    }}
                     key={index}
-                    title={word}
                     onPress={() => DeleteContent(index, words, setWords, setWordContent)}
-                />
+                >
+                    <Text style={{ color: 'black' }}>{word}</Text>
+
+                </TouchableOpacity>
+                // </View>
+
             ))
         )
         setWordContent(contentHolder)
@@ -80,17 +186,85 @@ function TagRecording(): JSX.Element {
             , time)
     }
 
-    return (
-        <View style={{alignItems: 'center'}}>
-            <WordContainer content={recordedContentHolder as JSX.Element[]}></WordContainer>
-            <Text>Speak to me, I'm listening to you</Text>
-            
 
-            <View  style={{width:250, height:100}}>
-                <Button
-                    title="Record"
-                    onPress={generateUserInputs} />
-            </View>
+    //TextMode
+    const [textInput, setTextInput] = useState<string>()
+    const onChangeText = (text: string) => {
+        setTextInput(text)
+    }
+
+    //switch mode state
+    const [currTypeButton, setCurrTypeButton] = useState<string>("Type Mode")
+    const focusOnInput = useRef<TextInput>(null)
+    const switchMode = () => {
+        if (currTypeButton === "Type Mode") {
+            focusOnInput.current?.focus();
+            setCurrTypeButton("Speech Mode")
+        } else {
+            setCurrTypeButton("Type Mode")
+            Keyboard.dismiss()
+        }
+        // updateModeConent()
+    }
+    useEffect(() => {
+        updateModeConent()
+    }, [currTypeButton])
+
+    const [modeContent, setModeContent] = useState<JSX.Element>(
+        <ModeContentComponents
+            textInput={textInput}
+            onChangeText={onChangeText}
+            recordTags={recordTags}
+            setTextInput={setTextInput}
+            currTypeButton={currTypeButton}
+            switchMode={switchMode}
+            focusOnInput={focusOnInput}
+        />
+    )
+    const updateModeConent = () => {
+        let ModeContentHolder = (
+            <ModeContentComponents
+                textInput={textInput}
+                onChangeText={onChangeText}
+                recordTags={recordTags}
+                setTextInput={setTextInput}
+                currTypeButton={currTypeButton}
+                switchMode={switchMode}
+                focusOnInput={focusOnInput}
+            />
+        )
+        setModeContent(ModeContentHolder)
+    }
+    return (
+        <View style={{ alignItems: 'center', backgroundColor: 'yellow' }}>
+            <ModifyContainer text="Save" />
+            <ModifyContainer text="Edit" />
+
+            <WordContainer content={recordedContentHolder as JSX.Element[]}></WordContainer>
+
+            {/* if currTypeButton === speech mode, it is currenlty Type mode so that by clicking that button it switches to speech mode */}
+            {/* The Type mode has Speech mode button and text input for user keyboard input. */}
+            {/* {currTypeButton === "Speech Mode" &&
+                
+            } */}
+            <ModeContentContainer content={modeContent as JSX.Element} />
+
+            {/* The speech mode has Type mode button, text input for cursor blinking only, and record button at the bottom. */}
+
+            {currTypeButton === "Type Mode" &&
+                <View style={{ width: '100%' }}>
+
+                    {/* STT 작동하게할것 */}
+                    <Button
+                        title="Record"
+                        onPress={generateUserInputs} />
+                </View>
+            }
+
+            {/* swtich mode button. 
+            Todo: 위의 높이를 100%로 해서 화면을 꽉 채우고, 이 버튼을 컴포넌트화해서 두 모드안에 넣자.
+            */}
+
             <WordContainer content={InputContentHolder as JSX.Element[]}></WordContainer>
         </View>
     )
